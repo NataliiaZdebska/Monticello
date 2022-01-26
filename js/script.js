300de0ab4ccf740611f1f7e09e2be190ce0da073
NataliiaZdebska <nata007ask@gmail.com>
2022-01-26 17:56:08 +0200

    corrected news section

--- js/script.js
+++ js/script.js
@@ -24,8 +24,28 @@ $(document).ready(function () {
         dots: false,
         slidesToShow: 3,
         slidesToScroll: 1,
-        centerMode: true,
-        centerPadding: '30px'
+        // centerMode: true,
+        // centerPadding: '0px',
+        responsive: [
+            {
+                breakpoint: 1600,
+                settings: {
+                    slidesToShow: 2
+                }
+            },
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 2
+                }
+            },
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 1
+                }
+            }
+            ]
     })
 })
 
